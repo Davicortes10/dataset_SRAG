40e67a7aacd7df5d0561dd49b18d94b10a15c681
DaviCortesDev <davicortes1@hotmail.com>
2025-01-22 01:20:00 -0300

    Classe Data_Lake

--- pre_processamento/main.py
+++ pre_processamento/main.py
@@ -1,5 +1,4 @@
 from pre_processamento import PreprocessDataset
 
-bot = PreprocessDataset("C:/Users/aleanse/Downloads/INFLUD20-01-05-2023.csv")
+bot = PreprocessDataset()
 bot.processar_tipos_colunas()
-bot.atualizar_Data_Lake()
--- pre_processamento/pre_processamento.py
+++ pre_processamento/pre_processamento.py
@@ -6,60 +6,10 @@ from pyspark.sql.types import StringType, StructField, StructType
 
 
 class PreprocessDataset:
-    def __init__(self,file_path):
-        self.file_path = file_path
-        self.db_connection = "jdbc:mysql://34.170.252.6:3306/srag_datalake"
+    def __init__(self):
         self.gcp_db_connection = "mysql+pymysql://devdavi:12345678@34.170.252.6/srag_datalake"
         self.conn = create_engine(self.gcp_db_connection)
         self.df = self.ler_gcp_DB()
-
-        self.spark = SparkSession.builder \
-            .appName("Atualizar Data Lake") \
-            .config("spark.jars", "/path/to/mysql-connector-java-8.0.13.jar") \
-        .getOrCreate()
-
-    def atualizar_Data_Lake(self):
-        print("Iniciando o processo de atualização do Data Lake com PySpark...")
-
-        # Leitura do arquivo CSV
-        try:
-            print(f"Lendo o arquivo CSV em: {self.file_path}")
-            df = self.spark.read.option("delimiter", ";") \
-                .option("header", "true") \
-                .csv(self.file_path)
-            print(f"Arquivo CSV lido com sucesso! Linhas: {df.count()}, Colunas: {len(df.columns)}")
-            print(f"Colunas detectadas: {df.columns}")
-            self.conectar_DB(df)
-        except Exception as e:
-            print(f"Erro ao ler o arquivo CSV: {e}")
-            return
-
-
-
-
-
-
-
-    def conectar_DB(self, df):
-        # Upload para o banco de dados
-        try:
-            print(f"Iniciando o upload para a tabela 'srag_datalake' no banco de dados...")
-            df.write \
-                .format("jdbc") \
-                .option("url", self.db_connection) \
-                .option("dbtable", "srag_datalake") \
-                .option("user", "devdavi") \
-                .option("password", "12345678") \
-                .option("driver", "com.mysql.cj.jdbc.Driver") \
-                .mode("overwrite") \
-                .save()
-            print("Upload concluído com sucesso!")
-        except Exception as e:
-            print(f"Erro ao fazer o upload para o banco de dados: {e}")
-            return
-
-        print("Processo de atualização do Data Lake finalizado com sucesso!")
-
 
     def ler_gcp_DB(self):
         # Escrevendo a consulta SQL para ler os dados da tabela
