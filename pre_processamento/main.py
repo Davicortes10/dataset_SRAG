212add2f3327521178095e279c4b8aac626242e7
aleanse dos santos lima rego <aleanse_54@hotmail.com>
2025-01-22 01:13:02 -0300

    processando tipos de coluna

--- pre_processamento/main.py
+++ pre_processamento/main.py
@@ -1,5 +1,5 @@
 from pre_processamento import PreprocessDataset
 
-bot = PreprocessDataset()
-
+bot = PreprocessDataset("C:/Users/aleanse/Downloads/INFLUD20-01-05-2023.csv")
+bot.processar_tipos_colunas()
 bot.atualizar_Data_Lake()
--- pre_processamento/pre_processamento.py
+++ pre_processamento/pre_processamento.py
@@ -11,15 +11,16 @@ class PreprocessDataset:
         self.db_connection = "jdbc:mysql://34.170.252.6:3306/srag_datalake"
         self.gcp_db_connection = "mysql+pymysql://devdavi:12345678@34.170.252.6/srag_datalake"
         self.conn = create_engine(self.gcp_db_connection)
+        self.df = self.ler_gcp_DB()
 
         self.spark = SparkSession.builder \
             .appName("Atualizar Data Lake") \
             .config("spark.jars", "/path/to/mysql-connector-java-8.0.13.jar") \
-            .getOrCreate()
-    
+        .getOrCreate()
+
     def atualizar_Data_Lake(self):
         print("Iniciando o processo de atualização do Data Lake com PySpark...")
-        
+
         # Leitura do arquivo CSV
         try:
             print(f"Lendo o arquivo CSV em: {self.file_path}")
@@ -32,6 +33,12 @@ class PreprocessDataset:
         except Exception as e:
             print(f"Erro ao ler o arquivo CSV: {e}")
             return
+
+
+
+
+
+
 
     def conectar_DB(self, df):
         # Upload para o banco de dados
@@ -52,13 +59,38 @@ class PreprocessDataset:
             return
 
         print("Processo de atualização do Data Lake finalizado com sucesso!")
-    
+
+
     def ler_gcp_DB(self):
         # Escrevendo a consulta SQL para ler os dados da tabela
         query = "SELECT * FROM srag_warehouse"
         pd.set_option("display.max_columns", None)
         # Lendo os dados para um DataFrame Pandas
         df = pd.read_sql(query, con=self.conn)
+        # Exibir informações iniciais sobre os dados
+        print("Primeiros registros:")
+        print(df.head())  # Visualizar os primeiros registros
+        print("\nInformações da tabela:")
+        print(df.info())  # Tipos de dados e valores ausentes
+        print("\nResumo estatístico:")
+        print(df.describe(include="all"))  # Resumo para valores numéricos e categóricos
+        return df
 
-        # Exibindo os primeiros registros
-        print(df.head())
+    def processar_tipos_colunas(self):
+        # Colunas do tipo 'data' (começam com 'DT')
+        date_columns = [col for col in self.df.columns if col.startswith("DT")]
+
+        # Conversão das colunas de data
+        for col in date_columns:
+            try:
+                self.df[col] = pd.to_datetime(self.df[col], errors="coerce",dayfirst=True)
+                print(f"Coluna '{col}' convertida para datetime.")
+            except Exception as e:
+                print(f"Erro ao converter coluna '{col}': {e}")
+
+        print("Conversão de tipos finalizada.")
+        print(self.df[date_columns].info())
+
+
+
+
