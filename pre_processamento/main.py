eec7900d596d35bfa6b070d2e2303c4df82979c7
aleanse dos santos lima rego <aleanse_54@hotmail.com>
2025-01-24 01:05:51 -0300

    processando colunas texto e numero(falta confirmar se esta funcionando)

--- pre_processamento/main.py
+++ pre_processamento/main.py
@@ -2,4 +2,6 @@ from pre_processamento import PreprocessDataset
 from data_lake import Data_Lake
 
 bot = PreprocessDataset()
-datalake = Data_Lake("/home/davicortes_oliveira1/dataset_SRAG/pre_processamento/srag.csv","srag_datalake")
+bot.executar_pipeline()
+
+#datalake = Data_Lake("/home/davicortes_oliveira1/dataset_SRAG/pre_processamento/srag.csv","srag_datalake")
--- pre_processamento/pre_processamento.py
+++ pre_processamento/pre_processamento.py
@@ -26,7 +26,21 @@ class PreprocessDataset:
         print(df.describe(include="all"))  # Resumo para valores numéricos e categóricos
         return df
 
-    def processar_tipos_colunas(self):
+    def remove_columns(self):
+        """
+        Remove colunas específicas de um DataFrame pandas.
+        """
+        columns_to_remove = ["TP_IDADE", "SEM_NOT", "SEM_PRI", "COD_IDADE", "CO_MUN_RES", "SURTO",
+                             "CO_RG_INTE", "CO_MU_INTE", "HISTO_VGM", "PCR_SARS2", "PAC_COCBO",
+                             "ID_REGIONA", "CO_MU_NOT", "CO_UNI_NOT", "CO_PAIS", "COD_RG_RESI",
+                             "SURTO_SG", "DT_RAIOX", "DT_ENCERRA", "PAIS_VGM", "CO_VGM", "LO_PS_VGM",
+                             "DT_RT_VGM", "DT_TOMO", "DT_RES_AN", "DT_CO_SOR", "DT_RES"]
+        # Filtra apenas as colunas que existem no DataFrame
+        existing_columns = [col for col in columns_to_remove if col in self.df.columns]
+        self.df = self.df.drop(columns=existing_columns)
+        print("As colunas foram removidas com sucesso.")
+
+    def processar_colunas_data(self):
         # Colunas do tipo 'data' (começam com 'DT')
         date_columns = [col for col in self.df.columns if col.startswith("DT")]
 
@@ -41,26 +55,50 @@ class PreprocessDataset:
         print("Conversão de tipos finalizada.")
         print(self.df[date_columns].info())
 
-    def remove_columns(self,df: pd.DataFrame) -> pd.DataFrame:
-    
-      """
-      Remove colunas específicas de um DataFrame pandas.
-    
-      Args:
-          df (pd.DataFrame): DataFrame de onde as colunas serão removidas.
-          columns_to_remove (list): Lista de nomes das colunas a serem removidas.
-    
-      Returns:
-          pd.DataFrame: DataFrame atualizado sem as colunas especificadas.
-      """
-      columns_to_remove = ["TP_IDADE", "SEM_NOT", "SEM_PRI", "COD_IDADE", "CO_MUN_RES", "SURTO", 
-        "CO_RG_INTE", "CO_MU_INTE", "HISTO_VGM", "PCR_SARS2", "PAC_COCBO", 
-        "ID_REGIONA", "CO_MU_NOT", "CO_UNI_NOT", "CO_PAIS", "COD_RG_RESI", 
-        "SURTO_SG", "DT_RAIOX", "DT_ENCERRA", "PAIS_VGM", "CO_VGM", "LO_PS_VGM", 
-        "DT_RT_VGM", "DT_TOMO", "DT_RES_AN", "DT_CO_SOR", "DT_RES"]
-      # Filtra apenas as colunas que existem no DataFrame
-      existing_columns = [col for col in columns_to_remove if col in df.columns]
-      return df.drop(columns=existing_columns)
+    """def processar_colunas_numericas(self):
+      AINDA VOU CONFIRMAR SE TÀ FUNCIONANDO, SE QUISER PODE IR TENTANDO AI
+        # Colunas numéricas (usando inferência de tipos)
+        numeric_columns = self.df.select_dtypes(include=['object']).columns
+        for col in numeric_columns:
+            try:
+                # Tenta converter para numérico, forçando erros a se tornarem NaN
+                self.df[col] = pd.to_numeric(self.df[col], errors='coerce')
+                print(f"Coluna '{col}' convertida para numérica.")
+            except Exception as e:
+                print(f"Erro ao converter coluna '{col}': {e}")
+    """
+    """def processar_colunas_texto(self):
+         AINDA VOU CONFIRMAR SE TÀ FUNCIONANDO, SE QUISER PODE IR TENTANDO AI
+        # Converte as colunas para texto (string), ou para categoria se necessário
+        for col in self.df.select_dtypes(include=['object']).columns:
+            try:
+                # Verifica se o tamanho único de valores na coluna é grande o suficiente para considerar como categoria
+                unique_vals = self.df[col].dropna().unique()
+                if unique_vals < 0.1 * len(self.df):
+                    self.df[col] = self.df[col].astype('category')  # Coluna com poucas categorias
+                    print(f"Coluna '{col}' convertida para 'category'.")
+                else:
+                    self.df[col] = self.df[col].astype(str)  # Caso contrário, converte para string
+                    print(f"Coluna '{col}' convertida para string.")
+            except Exception as e:
+                print(f"Erro ao converter coluna '{col}': {e}")
+    """
+
+
+    def executar_pipeline(self):
+        """
+        Executa todos os passos da pipeline em sequência.
+        """
+        print("Iniciando o processo de remoção de colunas...")
+        self.remove_columns()
+        print("\nIniciando o processo de conversão de tipos...")
+        self.processar_colunas_data()
+        self.processar_colunas_texto()
+        self.processar_colunas_numericas()
+        print(self.df.info())
+        print("\nPipeline executada com sucesso.")
+
+
 
 
 
